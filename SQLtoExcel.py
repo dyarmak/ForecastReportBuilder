2bc1d45c66616b90a2df407204d3d7cf538c5710
Dan Yarmak <dyarmak@h2safety.ca>
2019-04-23 14:07:13 -0600

    re-arranged structure of SQLtoExcel.py

--- SQLtoExcel.py
+++ SQLtoExcel.py
@@ -2,8 +2,8 @@
 # This script will query the DB and save the query results into excel files
 #*********************************************************************************
 
+## SQL Query export to Excel files
 
-## From SQL to DataFrame Pandas
 import os
 import pandas as pd
 import pyodbc
@@ -15,20 +15,18 @@ server = os.environ.get('DBServer')
 database = os.environ.get('DBName')
 username = os.environ.get('DBUsername')
 password = os.environ.get("DBPassword")
-# All of those need to be put into environ Vars before I upload to GitHub
+# Establish DB Connection
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-credDF = pd.read_sql(creditQuery, cnxn, index_col='SubProjectID')
-# export to .xlsx
-credDF.to_excel(creditFName, sheet_name='Sheet1')
-
-invoDF = pd.read_sql(invoicedQuery, cnxn, index_col='SubProjectID')
-# export to .xlsx
-invoDF.to_excel(invoicedFName, sheet_name='Sheet1')
-
+# Execute Queries
 foreDF = pd.read_sql(forecastQuery, cnxn, index_col='SubProjectID')
+invoDF = pd.read_sql(invoicedQuery, cnxn, index_col='SubProjectID')
+credDF = pd.read_sql(creditQuery, cnxn, index_col='SubProjectID')
+
+
 # export to .xlsx
 foreDF.to_excel(forecastFName, sheet_name='Sheet1')
-
+invoDF.to_excel(invoicedFName, sheet_name='Sheet1')
+credDF.to_excel(creditFName, sheet_name='Sheet1')
 
