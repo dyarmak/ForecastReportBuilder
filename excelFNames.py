eaa1a9e21421a32728be6227779d274acbcc3cbf
Dan Yarmak <dyarmak@h2safety.ca>
2019-05-08 12:56:04 -0600

    dated output folder added unformattedFName added outputFName

--- excelFNames.py
+++ excelFNames.py
@@ -1,8 +1,17 @@
+import datetime
+
+today = datetime.datetime.now().date()
+year = today.strftime("%Y")
+month = today.strftime("%m")
+day = today.strftime("%d")
+
 # FileName variables
 forecastFName = "qry_Forecast.xlsx"
 invoicedFName = "qry_Invoiced.xlsx"
 creditFName = "qry_Credits.xlsx"
 combinedFName = "Combined.xlsx"
+unformattedFName = "Unformatted.xlsx"
+outputFName = "Forecast" + year + month + day + ".xlsx"
 
 # Set to 1 to created logging files
 logMe = 0
