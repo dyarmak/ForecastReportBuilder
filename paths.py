88e853903c709f00e018e5e3fd3b3ef272e9971b
Dan Yarmak <dyarmak@h2safety.ca>
2019-05-31 16:11:17 -0600

    Added savePath int for multiple runs in a day

--- paths.py
+++ paths.py
@@ -6,6 +6,15 @@ savePath = today.strftime("%Y%m%d")
 
 startPath = os.getcwd()
 
+# Check if directory exists
+        # !Exists, Create folder without int
+        # Exists, create new with int appended
+num = 1
+while os.path.exists(savePath) is True:
+        savePath = today.strftime("%Y%m%d")
+        savePath = savePath + "-" + str(num)
+        num += 1
+
 if os.path.exists(savePath) is False:
         os.mkdir(savePath)
 
